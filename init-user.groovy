2b21da5f8d4bd3108d5feea9e8f3be8f3b068b19
nalbam <me@nalbam.com>
2018-03-21 16:36:04 +0900

    csrf

--- init-user.groovy
+++ init-user.groovy
@@ -4,11 +4,19 @@ import hudson.security.*
 def env = System.getenv()
 
 def jenkins = Jenkins.getInstance()
+
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+
+//jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+
+jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
+
+Set<String> agentProtocolsList = ['JNLP4-connect', 'Ping']
+jenkins.setAgentProtocols(agentProtocolsList)
 
 def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+
 jenkins.save()
