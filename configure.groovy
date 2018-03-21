d2d8f53ef3a111fddc47f215cc9cb976346ed284
nalbam <me@nalbam.com>
2018-03-21 17:21:43 +0900

    Logged

--- configure.groovy
+++ configure.groovy
@@ -21,16 +21,11 @@ jenkins.setCrumbIssuer(new hudson.security.csrf.DefaultCrumbIssuer(true))
 // Private Realm
 jenkins.setSecurityRealm(new hudson.security.HudsonPrivateSecurityRealm(false))
 
-// Logged In User Access
-def strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-jenkins.setAuthorizationStrategy(strategy)
-
 // Create User
 def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 
-// Admin
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+// Authorization
+jenkins.setAuthorizationStrategy(new hudson.security.FullControlOnceLoggedInAuthorizationStrategy())
 
 jenkins.save()
